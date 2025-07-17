3aa54b0d569f953fa941ae7192096433a59a4a57
Lshiroc <zeynalmerdanli4321@gmail.com>
2025-07-18 01:06:49 +0400

    made it into a class and a cli tool

--- main.js
+++ main.js
@@ -1,43 +1,102 @@
+#!/usr/bin/env node
+
 import process from "node:process";
 import readline from "readline";
+import fs from "node:fs"
 
-readline.emitKeypressEvents(process.stdin);
-console.clear();
-if (process.stdin.setRawMode !== null) {
-    process.stdin.setRawMode(true);
+class CLIJS {
+	row = 1;
+	col = 1;
+	mode = "";
+
+	start(filename) {
+		readline.emitKeypressEvents(process.stdin);
+		console.clear();
+		if (process.stdin.setRawMode !== null) {
+			process.stdin.setRawMode(true);
+		}
+	
+		if (filename) {
+			let buf;
+			fs.readFile(filename, 'utf8', (err, data) => {
+				if (err) {
+					console.error(err);
+					return;
+				}
+				buf = Buffer.from(data, "utf8");
+
+				console.log(data);
+			})
+
+			console.log(buf);
+		}
+
+		const handleKeypress = (c, k) => {
+			let keymap = this.keymaps.find(km => km.key === k.name);
+			if (keymap) {
+				keymap.action();
+			}
+		}
+
+		process.stdin.on("keypress", handleKeypress);
+	}
+
+	actions = {
+		moveCursorTop: () => {
+			readline.moveCursor(process.stdin, 0, -1);
+			if (this.row != 1) this.row--;
+		},
+		moveCursorBottom: () => {
+			readline.moveCursor(process.stdin, 0, 1);
+			this.row++;
+		},
+		moveCursorLeft: () => {
+			readline.moveCursor(process.stdin, -1, 0);
+			if (this.col != 1) this.col--;
+		},
+		moveCursorRight: () => {
+			readline.moveCursor(process.stdin, 1, 0);
+			this.col++;
+		},
+		quit: () => {
+			readline.moveCursor(process.stdin, -this.col, -this.row);
+			process.exit();
+			console.clear();
+		},
+		insert: () => {
+			this.mode = "insert";
+		}
+	}
+
+	keymaps = [
+		{
+			key: "k",
+			action: this.actions.moveCursorTop
+		},
+		{
+			key: "j",
+			action: this.actions.moveCursorBottom
+		},
+		{
+			key: "h",
+			action: this.actions.moveCursorLeft
+		},
+		{
+			key: "l",
+			action: this.actions.moveCursorRight
+		},
+		{
+			key: "q",
+			action: this.actions.quit
+		},
+		{
+			key: "i",
+			action: this.actions.insert
+		}
+	]
 }
 
-process.stdout.write("text\r");
-console.log("this is a test text");
-console.log("and this is another one ");
-const handleKeypress = (c, k) => {
-    switch(k.name) {
-        case "k":
-            readline.moveCursor(process.stdin, 0, -1);
-            break;
-        case "j":
-            readline.moveCursor(process.stdin, 0, 1);
-            break;
-        case "h":
-            readline.moveCursor(process.stdin, -1, 0);
-            break;
-        case "l":
-            readline.moveCursor(process.stdin, 1, 0);
-            break;
-        case "q":
-            console.clear();
-            process.exit();
-            break;
-    }
-}
+const clijs = new CLIJS();
 
-process.stdin.on("keypress", handleKeypress);
-
-/* setTimeout(() => {
-    process.stdout.write("text changed!!!\n");
-    if (process.stdin.setRawMode !== null) {
-        process.stdin.setRawMode(false);
-        // process.stdin.off("keypress", handleKeypress);
-    }
-}, 2000); */
+clijs.start(process.argv[2]);
 
